c479110ab651e1acd0da8651045e6ced2417f0fb
Feleks_Webs <49903400+FeleksWebs@users.noreply.github.com>
2020-07-11 12:42:40 +0100

    Uploading Build Files

---
+++ build/precache-manifest.704dbb88c86f2237473a01e697a84341.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "327c56c26ae3ee4514f68aa3c2813273",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92f502d880e3985ea0d0",
+    "url": "/static/css/main.d7d3422a.chunk.css"
+  },
+  {
+    "revision": "87c3034490242feafeca",
+    "url": "/static/js/2.8c576618.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.8c576618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92f502d880e3985ea0d0",
+    "url": "/static/js/main.2e3800e9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
